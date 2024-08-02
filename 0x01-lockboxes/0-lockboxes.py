16f6213a7be36943d458713c9d2019bff8b736d9
vicano-code <vc.anokwuru@gmail.com>
2024-08-02 13:14:25 +0100

    PEP8 style correction

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-You have n number of locked boxes in front of you. Each box is numbered 
+You have n number of locked boxes in front of you. Each box is numbered
 sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 Write a method that determines if all the boxes can be opened.
 - Prototype: def canUnlockAll(boxes)
@@ -12,29 +12,31 @@ Write a method that determines if all the boxes can be opened.
 - Return True if all boxes can be opened, else return False
 """
 
+
 def canUnlockAll(boxes):
-  boxesSize = len(boxes)
+    """ check if all keys for every box in boxes exist """
+    boxesSize = len(boxes)
 
-  # remove keys from boxes with its own key inside
-  # and concatenate the lists in boxes list
-  key = 0
-  newList = []
-  for keyList in boxes:
-    if key in keyList:
-      keyList.remove(key)
-    newList = [*newList, *keyList]
-    key += 1
-  
-  # sort the newList
-  sortedNewList = sorted(newList)
+    # remove keys from boxes with its own key inside
+    # and concatenate the lists in boxes list
+    key = 0
+    newList = []
+    for keyList in boxes:
+        if key in keyList:
+            keyList.remove(key)
+        newList = [*newList, *keyList]
+        key += 1
 
-  # Get unique List using set
-  uniqueKeyList = set(sortedNewList)
+    # sort the newList
+    sortedNewList = sorted(newList)
 
-  # Check if keys for all boxes are present in uniqueKeyList
-  # note that first box boxes[0] is unlocked so start from key 1
-  for i in range(1, boxesSize):
-    if i not in uniqueKeyList:
-      return False
-  
-  return True
+    # Get unique List using set
+    uniqueKeyList = set(sortedNewList)
+
+    # Check if keys for all boxes are present in uniqueKeyList
+    # note that first box boxes[0] is unlocked so start from key 1
+    for i in range(1, boxesSize):
+        if i not in uniqueKeyList:
+            return False
+
+    return True
