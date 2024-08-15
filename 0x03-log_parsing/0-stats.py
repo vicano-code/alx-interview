7ceeff2fb7b4ed0f6dacb30cb17506e200e4d978
Victor Anokwuru <vc.anokwuru@gmail.com>
2024-08-15 10:50:34 -0700

    review code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -19,20 +19,18 @@ for line in sys.stdin:
     line_list = line.split()
 
     # check line_list to ensure it meets the required size or format
-    if len(line_list) > 8:
+    if len(line_list) == 9:
         # obtain file size from line_list and cummulate
         try:
             file_size += int(line_list[-1])
 
             # obtain status code count
             status_code[line_list[-2]] += 1
-
-            # print stats after every 10 lines
-            if counter == 10:
-                print(f"File size: {file_size}")
-                for key, val in status_code.items():
-                    if val != 0:
-                        print(f"{key}: {val}")
-                counter = 0
-        except (ValueError, IndexError):
-            pass  # ignore line and got to next line
+        except Exception:
+            pass
+    # print stats after every 10 lines
+    if counter % 10 == 0:
+        print(f"File size: {file_size}")
+        for key, val in status_code.items():
+            if val != 0:
+                print(f"{key}: {val}")
