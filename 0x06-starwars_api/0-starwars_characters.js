bd2df251585d344a1e1d7ef5e81d335feb9842dc
Victor Anokwuru <vc.anokwuru@gmail.com>
2024-09-05 15:50:28 -0700

    refactor code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,26 +3,35 @@
 const id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
 const request = require('request');
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  if (response.statusCode === 200) {
-    const result = [];
-    result.push(body);
-    r = JSON.parse(result);
-    for (const i in r.characters) {
-      request(r.characters[i], function (err, response, body) {
-        if (err) {
-	  console.log(err);
-        }
-        if (response.statusCode === 200) {
-	  console.log(JSON.parse(body).name);
-        }
-      });
+const util = require('util');
+
+const requestPromise = util.promisify(request);
+
+async function getCharacterNames() {
+  try {
+    const response = await requestPromise(url);
+    if (response.statusCode !== 200) {
+      console.log('Error code: ' + response.statusCode);
+      return;
     }
-  } else {
-    console.log('Error code: ' + response.statusCode);
+
+    const characters = JSON.parse(response.body).characters;
+    for (const characterUrl of characters) {
+      try {
+        const charResponse = await requestPromise(characterUrl);
+        if (charResponse.statusCode === 200) {
+          const character = JSON.parse(charResponse.body).name;
+          console.log(character);
+        } else {
+          console.log('Error code: ' + charResponse.statusCode);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  } catch (err) {
+    console.log(err);
   }
-});
+}
+
+getCharacterNames();
